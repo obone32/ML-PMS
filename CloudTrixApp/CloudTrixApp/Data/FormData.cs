44c753c64593fb84c53780b761dcc77d406f7283
MinalShinde0510 <minal0510@GMAIL.COM>
2020-02-14 18:27:37 +0530

    login
    login design Changes

---
+++ CloudTrixApp/CloudTrixApp/Data/FormData.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace CloudTrixApp.Data
+{
+    public class FormData
+    {
+        public static DataTable SelectAll()
+        {
+            SqlConnection connection = PMMSData.GetConnection();
+            string selectProcedure = "[FormSelectAll]";
+            SqlCommand selectCommand = new SqlCommand(selectProcedure, connection);
+            selectCommand.CommandType = CommandType.StoredProcedure;
+            DataTable dt = new DataTable();
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = selectCommand.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    dt.Load(reader);
+                }
+                reader.Close();
+            }
+            catch (SqlException)
+            {
+                return dt;
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return dt;
+        }
+    }
+}
---
+++ CloudTrixApp/CloudTrixApp/Models/FormController.cs
@@ -0,0 +1,107 @@
+﻿using CloudTrixApp.Data;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CloudTrixApp.Models
+{
+    public class FormController : Controller
+    {
+        DataTable dtForm = new DataTable();
+
+        //
+        // GET: /Form/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // GET: /Form/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        //
+        // GET: /Form/Create
+        public ActionResult Create()
+        {
+            dtForm = FormData.SelectAll();
+            //foreach (var item in dtForm.Columns)
+            //{
+            //   item =item(s=>s.)
+            //}
+            ViewData.Model = dtForm.AsEnumerable();
+            return View();
+        }
+
+        //
+        // POST: /Form/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Form/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Form/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Form/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Form/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
