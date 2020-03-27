397e6d2045381f893d05bc0ba53edc416e7c0bf6
MinalShinde0510 <minal0510@GMAIL.COM>
2020-03-27 11:56:06 +0530

    Dashboard
    Dashboard work

--- CloudTrixApp/CloudTrixApp/Controllers/HomeController.cs
+++ CloudTrixApp/CloudTrixApp/Controllers/HomeController.cs
@@ -1,8 +1,12 @@
 using CloudTrixApp.Data;
+using CloudTrixApp.Models;
+using CloudTrixApp.Models.ViewModel;
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
+using System.Reflection;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Security;
@@ -36,16 +40,117 @@ namespace CloudTrixApp.Controllers
             }
             TempData["RemoveTab"] = RemoveTab;
             TempData["ShowTab"] = ShowTab;
+            LoadDataCount();
             return View();
         }
+        public void LoadDataCount()
+        {
+            //Project
+            DataTable dtProject = Data.ProjectData.SelectAll();
+            TempData["TotalPRoject"] = dtProject.Rows.Count;
+            //Load Project data
+            if (dtProject.Rows.Count > 0)
+            {
+                List<Project> ProjectDataDetails = new List<Project>();
+                ProjectDataDetails = DataTableToList<Project>(dtProject);
+                ViewBag.ProjectDetailsList = ProjectDataDetails;
+            }
+            //==================
+            //Company
+            DataTable dtCompany = Data.CompanyData.SelectAll();
+            TempData["TotalCompany"] = dtCompany.Rows.Count;
+            //Load Company data
+            if (dtCompany.Rows.Count > 0)
+            {
+                List<Company> CompanyDataDetails = new List<Company>();
+                CompanyDataDetails = DataTableToList<Company>(dtCompany);
+                ViewBag.CompanyDetailsList = CompanyDataDetails;
+            }
+            //==================
+            //Client
+            DataTable dtClient = Data.ClientData.SelectAll();
+            TempData["TotalClient"] = dtClient.Rows.Count;
+            //Load Client data
+            if (dtClient.Rows.Count > 0)
+            {
+                List<Client> ClientDataDetails = new List<Client>();
+                ClientDataDetails = DataTableToList<Client>(dtClient);
+                ViewBag.ClientDetailsList = ClientDataDetails;
+            }
+            //==================
+            //Employee
+            DataTable dtEmployee = Data.EmployeeData.SelectAll();
+            TempData["TotalEmployee"] = dtEmployee.Rows.Count;
+            //Load Employee data
+            if (dtEmployee.Rows.Count > 0)
+            {
+                List<CloudTrixApp.Models.Employee> EmployeeDataDetails = new List<CloudTrixApp.Models.Employee>();
+                EmployeeDataDetails = DataTableToList<CloudTrixApp.Models.Employee>(dtEmployee);
+                ViewBag.EmployeeDetailsList = EmployeeDataDetails;
+            }
+            //==================
+            //Invoice
+            DataTable dtInvoice = Data.InvoiceData.SelectAll();
+            TempData["TotalInvoice"] = dtInvoice.Rows.Count;
+            //Load Invoice data
+            if (dtInvoice.Rows.Count > 0)
+            {
+                List<Invoice> InvoiceDataDetails = new List<Invoice>();
+                InvoiceDataDetails = DataTableToList<Invoice>(dtInvoice);
+                ViewBag.InvoiceDetailsList = InvoiceDataDetails;
+            }
+            //==================
+            //Task Management
+            DataTable dtTaskManagement = Data.TaskAssignmentData.SelectAll();
+            TempData["TotalTaskManagement"] = dtTaskManagement.Rows.Count;
+            //Load Invoice data
+            if (dtTaskManagement.Rows.Count > 0)
+            {
+              //  List<Task_Details> TaskDataDetails = new List<Task_Details>();
+                List<DataRow> rows = dtTaskManagement.Rows.Cast<DataRow>().ToList();
+              //  TaskDataDetails = DataTableToList<Task_Details>(dtTaskManagement);
+                ViewBag.TaskManagementDetailsList = rows.ToList();
+            }
+            //SqlConnection connection = PMMSData.GetConnection();
+            //var sql = "Task_Details";
+            //var multi = connection.QueryMultiple(sql);
+            //DataTable dtTaskManagement = Data.TaskAssignmentData.SelectAll();
+            //TempData["TotalTaskManagement"] = dtTaskManagement.Rows.Count;
+            ////Load Invoice data
+            //if (dtTaskManagement.Rows.Count > 0)
+            //{
+            //    Task_Details ObjTask_Details = new Task_Details();
 
+            //    //Assigning each Multiple tables data to specific single model class  
+            //    ObjTask_Details.clasTask = connection.Read<Task>().ToList();
+            //    ObjTask_Details.clasTaskState = objDetails.Read<TaskState>().ToList();
+            //    ObjTask_Details.clsTaskAssignment = objDetails.Read<TaskAssignment>().ToList();
+
+            //    List<MasterDetails> CustomerObj = new List<MasterDetails>();  
+
+            //    //List<Task_Details> TaskDataDetails = new List<Task_Details>();
+            //    //TaskDataDetails = DataTableToList<Task_Details>(dtTaskManagement);
+            //    //ViewBag.TaskManagementDetailsList = TaskDataDetails;
+            //}
+            //==================
+            //Time Sheet
+            DataTable dtTimeSheet = Data.TimesheetData.SelectAll();
+            TempData["TotalTimeSheet"] = dtTimeSheet.Rows.Count;
+            //Load Time Sheet data
+            if (dtTimeSheet.Rows.Count > 0)
+            {
+                List<Timesheet> TimesheetDetails = new List<Timesheet>();
+                TimesheetDetails = DataTableToList<Timesheet>(dtTimeSheet);
+                ViewBag.TimeSheetDetailsList = TimesheetDetails;
+            }
+        }
         [Authorize]
         public ActionResult Profile()
         {
             return View();
         }
 
-      
+
         [HttpPost]
         [Authorize]
         public ActionResult Logout()
@@ -54,8 +159,40 @@ namespace CloudTrixApp.Controllers
             Session.Clear();
             Session.RemoveAll();
             Session.Abandon();
-            return RedirectToAction("Create","Login");
+            return RedirectToAction("Create", "Login");
+        }
+        public static List<T> DataTableToList<T>(DataTable dt) where T : class, new()
+        {
+            List<T> lstItems = new List<T>();
+            if (dt != null && dt.Rows.Count > 0)
+                foreach (DataRow row in dt.Rows)
+                    lstItems.Add(ConvertDataRowToGenericType<T>(row));
+            else
+                lstItems = null;
+            return lstItems;
+        }
+        private static T ConvertDataRowToGenericType<T>(DataRow row) where T : class,new()
+        {
+            Type entityType = typeof(T);
+            T objEntity = new T();
+            foreach (DataColumn column in row.Table.Columns)
+            {
+                object value = row[column.ColumnName];
+                if (value == DBNull.Value) value = null;
+                PropertyInfo property = entityType.GetProperty(column.ColumnName, BindingFlags.Instance | BindingFlags.IgnoreCase | BindingFlags.Public);
+                try
+                {
+                    if (property != null && property.CanWrite)
+                        property.SetValue(objEntity, value, null);
+
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+            return objEntity;
         }
     }
 }
- 
+
---
+++ CloudTrixApp/CloudTrixApp/Models/ViewModel/Task_Details.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CloudTrixApp.Models.ViewModel
+{
+    public class Task
+    {
+        public Int32 TaskID { get; set; }
+        public String TaskName { get; set; }
+        public String Description { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+    public class TaskAssignment
+    {
+        public Int32 TaskAssignmentID { get; set; }
+        public DateTime AssignmentDate { get; set; }
+        public Int32 TaskID { get; set; }
+        public Int32 EmployeeID { get; set; }
+        public Int32 TaskStateID { get; set; }
+        // ComboBox
+        public virtual Task Task { get; set; }
+        public virtual Employee Employee { get; set; }
+        public virtual TaskState TaskState { get; set; }
+    }
+    public class Employee
+    {
+        public Int32 EmployeeID { get; set; }
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public DateTime DOB { get; set; }
+        public DateTime DOJ { get; set; }
+        public String Gender { get; set; }
+        public String EMail { get; set; }
+        public String Mobile { get; set; }
+        public String Address1 { get; set; }
+        public String Address2 { get; set; }
+        public Decimal Salary { get; set; }
+        public String SignatureURL { get; set; }
+        public String UserName { get; set; }
+        public String Password { get; set; }
+        public Int32 CompanyID { get; set; }
+        public Int32 UserTypeID { get; set; }
+        public Int32 AddUserID { get; set; }
+        public DateTime AddDate { get; set; }
+        public Int32? ArchiveUserID { get; set; }
+        public DateTime? ArchiveDate { get; set; }
+        public bool IsActive { get; set; }
+        public bool Active { get; set; }
+        // ComboBox
+        public virtual Company Company { get; set; }
+        public virtual UserType UserType { get; set; }
+    }
+    public class TaskState
+    {
+        public Int32 TaskStateID { get; set; }
+        public String TaskStateName { get; set; }
+    }
+    public class Task_Details
+    {
+        public List<Task> clasTask { get; set; }
+        public List<TaskState> clasTaskState { get; set; }
+        public List<TaskAssignment> clsTaskAssignment { get; set; }
+    }
+
+}
